b43114e3eaabfbc90c2b7e8e81bf84e1b16f62d9
kumarraju95 <pansariraju489@gmail.com>
2026-08-14 14:48:39 +0530

    error solving

--- server.js
+++ server.js
@@ -78,6 +78,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
